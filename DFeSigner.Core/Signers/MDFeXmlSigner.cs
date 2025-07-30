e111ae745a701dbfd4a7572dd9af1e4cf051a2e8
tiago-saldanha-sige <tiago.saldanha@sigeatende.com.br>
2025-07-29 21:54:25 -0300

    feat: Adiciona private const strings para classes de DFe (MDF-e, NF-e, NFC-e)

---
+++ DFeSigner.Core/Signers/MDFeXmlSigner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Xml;
+
+namespace DFeSigner.Core.Signers
+{
+    /// <summary>
+    /// Implementação concreta da assinatura digital para MDF-e.
+    /// </summary>
+    public class MDFeXmlSigner : DFeXmlSigner
+    {
+        private static readonly string NFeNamespace = "http://www.portalfiscal.inf.br/mdfe";
+        private static readonly string PrefixNFeNamespace = "mdfe";
+
+        /// <summary>
+        /// Implementação específica para MDF-e para identificar o elemento 'infMDFe' a ser assinado.
+        /// </summary>
+        /// <param name="document">O objeto XmlDocument contendo o XML da MDF-e.</param>
+        /// <returns>Uma tupla contendo o XmlElement 'infMDFe' e o seu atributo 'Id'.</returns>
+        /// <exception cref="InvalidOperationException">Lançada se o elemento 'infMDFe' ou seu 'Id' não for encontrado,
+        /// ou se o XML não for identificado como um MDF-e (modelo 58).</exception>
+        protected override string GetReferenceId(XmlDocument document)
+        {
+            XmlNamespaceManager ns = new(document.NameTable);
+            ns.AddNamespace(PrefixNFeNamespace, NFeNamespace);
+
+            XmlElement ideElement = document.GetElementsByTagName("ide")[0] as XmlElement;
+            if (ideElement == null)
+            {
+                throw new InvalidOperationException("Elemento 'ide' não encontrado no XML. O XML pode não ser um documento fiscal válido.");
+            }
+
+            string modeloDocumento = document.GetElementsByTagName("mod")[0].InnerText;
+            if (modeloDocumento != "58")
+            {
+                throw new InvalidOperationException($"O XML fornecido não é um MDF-e (modelo 58). Modelo encontrado: {modeloDocumento}.");
+            }
+
+            XmlElement elementToSign = document.GetElementsByTagName("infMDFe")[0] as XmlElement;
+            if (elementToSign == null)
+            {
+                throw new InvalidOperationException($"Elemento '{PrefixNFeNamespace}:infMDFe' não encontrado no XML. Verifique se o XML é um MDF-e válida.");
+            }
+
+            string referenceId = elementToSign.Attributes["Id"]?.Value;
+            if (string.IsNullOrWhiteSpace(referenceId))
+            {
+                throw new InvalidOperationException($"Atributo 'Id' não encontrado ou vazio no elemento '{PrefixNFeNamespace}:infMDFe'.");
+            }
+
+            return referenceId;
+        }
+    }
+}
---
+++ DFeSigner.Tests/MDFeXmlSignerTests.cs
@@ -0,0 +1,112 @@
+﻿using DFeSigner.Core.Signers;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DFeSigner.Tests
+{
+    public class MDFeXmlSignerTests
+    {
+        private readonly string _mdfePath = Path.Combine(AppContext.BaseDirectory, "Xml", "mdfe.xml");
+        private readonly string _nfePath = Path.Combine(AppContext.BaseDirectory, "Xml", "nfe.xml");
+        private readonly string _certificatePath = Path.Combine(AppContext.BaseDirectory, "Certificates", "certificate.pfx");
+        private readonly string _certificateInvalidPath = Path.Combine(AppContext.BaseDirectory, "Certificates", "certificate.cer");
+        private readonly string _certificatePassword = "123";
+
+        private const string InvalidXmlWithoutReferenceId = "<MDFe xmlns=\"http://www.portalfiscal.inf.br/mdfe\"><infMDFe versao=\"3.00\" ><ide><cUF>43</cUF><tpAmb>2</tpAmb><tpEmit>2</tpEmit><mod>58</mod><serie>1</serie><nMDF>417</nMDF><cMDF>94986538</cMDF><cDV>6</cDV><modal>1</modal><dhEmi>2023-09-25T14:25:00-03:00</dhEmi><tpEmis>1</tpEmis><procEmi>0</procEmi><verProc>_5_55</verProc><UFIni>RS</UFIni><UFFim>SP</UFFim><infMunCarrega><cMunCarrega>4314902</cMunCarrega><xMunCarrega>Porto Alegre</xMunCarrega></infMunCarrega><infPercurso><UFPer>SC</UFPer></infPercurso><infPercurso><UFPer>PR</UFPer></infPercurso><dhIniViagem>2021-07-21T00:00:00-03:00</dhIniViagem></ide><emit><CNPJ>12345678901234</CNPJ><IE>9999999999</IE><xNome>TRANSPORTADORA FICTICIA LTDA</xNome><xFant>TRANSFICT</xFant><enderEmit><xLgr>Rua Ficticia do Emissor</xLgr><nro>1234</nro><xBairro>Bairro Central</xBairro><cMun>4314902</cMun><xMun>Porto Alegre</xMun><CEP>90000000</CEP><UF>RS</UF><fone>51999999999</fone><email>contato@ficticia.com.br</email></enderEmit></emit><infModal versaoModal=\"3.00\"><rodo><veicTracao><placa>ABC5678</placa><tara>400</tara><condutor><xNome>MOTORISTA FICTICIO</xNome><CPF>99999999999</CPF></condutor><tpRod>01</tpRod><tpCar>01</tpCar><UF>RS</UF></veicTracao></rodo></infModal><infDoc><infMunDescarga><cMunDescarga>4314902</cMunDescarga><xMunDescarga>Porto Alegre</xMunDescarga><infNFe><chNFe>43210712345678901234550030000015091684574043</chNFe></infNFe></infMunDescarga></infDoc><tot><qNFe>1</qNFe><vCarga>100.00</vCarga><cUnid>01</cUnid><qCarga>12.0000</qCarga></tot></infMDFe><infMDFeSupl><qrCodMDFe>https://dfe-portal.svrs.rs.gov.br/mdfe/qrCode?chMDFe=43230912345678901234580010000004171949865386&amp;tpAmb=2</qrCodMDFe></infMDFeSupl></MDFe>";
+        private const string InvalidXmlWithoutInfMDFeElement = "<MDFe xmlns=\"http://www.portalfiscal.inf.br/mdfe\"><infMDFa Id=\"MDFe43230912345678901234580010000004171949865386\" versao=\"3.00\" ><ide><cUF>43</cUF><tpAmb>2</tpAmb><tpEmit>2</tpEmit><mod>58</mod><serie>1</serie><nMDF>417</nMDF><cMDF>94986538</cMDF><cDV>6</cDV><modal>1</modal><dhEmi>2023-09-25T14:25:00-03:00</dhEmi><tpEmis>1</tpEmis><procEmi>0</procEmi><verProc>_5_55</verProc><UFIni>RS</UFIni><UFFim>SP</UFFim><infMunCarrega><cMunCarrega>4314902</cMunCarrega><xMunCarrega>Porto Alegre</xMunCarrega></infMunCarrega><infPercurso><UFPer>SC</UFPer></infPercurso><infPercurso><UFPer>PR</UFPer></infPercurso><dhIniViagem>2021-07-21T00:00:00-03:00</dhIniViagem></ide><emit><CNPJ>12345678901234</CNPJ><IE>9999999999</IE><xNome>TRANSPORTADORA FICTICIA LTDA</xNome><xFant>TRANSFICT</xFant><enderEmit><xLgr>Rua Ficticia do Emissor</xLgr><nro>1234</nro><xBairro>Bairro Central</xBairro><cMun>4314902</cMun><xMun>Porto Alegre</xMun><CEP>90000000</CEP><UF>RS</UF><fone>51999999999</fone><email>contato@ficticia.com.br</email></enderEmit></emit><infModal versaoModal=\"3.00\"><rodo><veicTracao><placa>ABC5678</placa><tara>400</tara><condutor><xNome>MOTORISTA FICTICIO</xNome><CPF>99999999999</CPF></condutor><tpRod>01</tpRod><tpCar>01</tpCar><UF>RS</UF></veicTracao></rodo></infModal><infDoc><infMunDescarga><cMunDescarga>4314902</cMunDescarga><xMunDescarga>Porto Alegre</xMunDescarga><infNFe><chNFe>43210712345678901234550030000015091684574043</chNFe></infNFe></infMunDescarga></infDoc><tot><qNFe>1</qNFe><vCarga>100.00</vCarga><cUnid>01</cUnid><qCarga>12.0000</qCarga></tot></infMDFa><infMDFeSupl><qrCodMDFe>https://dfe-portal.svrs.rs.gov.br/mdfe/qrCode?chMDFe=43230912345678901234580010000004171949865386&amp;tpAmb=2</qrCodMDFe></infMDFeSupl></MDFe>";
+        private const string InvalidXmlWithoutIdeElement = "<MDFe xmlns=\"http://www.portalfiscal.inf.br/mdfe\"><infMDFe Id=\"MDFe43230912345678901234580010000004171949865386\" versao=\"3.00\" ><ida><cUF>43</cUF><tpAmb>2</tpAmb><tpEmit>2</tpEmit><mod>58</mod><serie>1</serie><nMDF>417</nMDF><cMDF>94986538</cMDF><cDV>6</cDV><modal>1</modal><dhEmi>2023-09-25T14:25:00-03:00</dhEmi><tpEmis>1</tpEmis><procEmi>0</procEmi><verProc>_5_55</verProc><UFIni>RS</UFIni><UFFim>SP</UFFim><infMunCarrega><cMunCarrega>4314902</cMunCarrega><xMunCarrega>Porto Alegre</xMunCarrega></infMunCarrega><infPercurso><UFPer>SC</UFPer></infPercurso><infPercurso><UFPer>PR</UFPer></infPercurso><dhIniViagem>2021-07-21T00:00:00-03:00</dhIniViagem></ida><emit><CNPJ>12345678901234</CNPJ><IE>9999999999</IE><xNome>TRANSPORTADORA FICTICIA LTDA</xNome><xFant>TRANSFICT</xFant><enderEmit><xLgr>Rua Ficticia do Emissor</xLgr><nro>1234</nro><xBairro>Bairro Central</xBairro><cMun>4314902</cMun><xMun>Porto Alegre</xMun><CEP>90000000</CEP><UF>RS</UF><fone>51999999999</fone><email>contato@ficticia.com.br</email></enderEmit></emit><infModal versaoModal=\"3.00\"><rodo><veicTracao><placa>ABC5678</placa><tara>400</tara><condutor><xNome>MOTORISTA FICTICIO</xNome><CPF>99999999999</CPF></condutor><tpRod>01</tpRod><tpCar>01</tpCar><UF>RS</UF></veicTracao></rodo></infModal><infDoc><infMunDescarga><cMunDescarga>4314902</cMunDescarga><xMunDescarga>Porto Alegre</xMunDescarga><infNFe><chNFe>43210712345678901234550030000015091684574043</chNFe></infNFe></infMunDescarga></infDoc><tot><qNFe>1</qNFe><vCarga>100.00</vCarga><cUnid>01</cUnid><qCarga>12.0000</qCarga></tot></infMDFe><infMDFeSupl><qrCodMDFe>https://dfe-portal.svrs.rs.gov.br/mdfe/qrCode?chMDFe=43230912345678901234580010000004171949865386&amp;tpAmb=2</qrCodMDFe></infMDFeSupl></MDFe>";
+
+        [Fact]
+        public void Sign_ValidMDFeXmlAndCertificate_ReturnsSignedXml()
+        {
+            string xmlContent = File.ReadAllText(_mdfePath);
+            Assert.False(string.IsNullOrWhiteSpace(xmlContent), "O conteúdo do XML de exemplo da MDF-e não pode ser vazio.");
+
+            X509Certificate2 certificate = new X509Certificate2(_certificatePath, _certificatePassword, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+            Assert.NotNull(certificate);
+
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            string signedXml = signer.Sign(xmlContent, certificate);
+
+            Assert.False(string.IsNullOrWhiteSpace(signedXml));
+            Assert.Contains("<Signature", signedXml);
+        }
+
+        [Fact]
+        public void Sign_InvalidXmlContentWithoutReferenceId_ThrowsInvalidOperationException()
+        {
+            string invalidXml = InvalidXmlWithoutReferenceId;
+
+            X509Certificate2 certificate = new X509Certificate2(_certificatePath, _certificatePassword, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            Assert.Throws<InvalidOperationException>(() => signer.Sign(invalidXml, certificate));
+        }
+
+        [Fact]
+        public void Sign_InvalidXmlContentWithoutElementInfMDFe_ThrowsInvalidOperationException()
+        {
+            string invalidXml = InvalidXmlWithoutInfMDFeElement;
+
+            X509Certificate2 certificate = new X509Certificate2(_certificatePath, _certificatePassword, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            Assert.Throws<InvalidOperationException>(() => signer.Sign(invalidXml, certificate));
+        }
+
+        [Fact]
+        public void Sign_InvalidXmlContentWithoutElementIde_ThrowsInvalidOperationException()
+        {
+            string invalidXml = InvalidXmlWithoutIdeElement;
+
+            X509Certificate2 certificate = new X509Certificate2(_certificatePath, _certificatePassword, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            Assert.Throws<InvalidOperationException>(() => signer.Sign(invalidXml, certificate));
+        }
+
+        [Fact]
+        public void Sign_InvalidXmlContent_ThrowsArgumentException()
+        {
+            string invalidXml = "";
+            X509Certificate2 certificate = new X509Certificate2(_certificatePath, _certificatePassword, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            Assert.Throws<ArgumentException>(() => signer.Sign(invalidXml, certificate));
+        }
+
+        [Fact]
+        public void Sign_NullCertificate_ThrowsArgumentNullException()
+        {
+            string xmlContent = File.ReadAllText(_mdfePath);
+            X509Certificate2 nullCertificate = null;
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            Assert.Throws<ArgumentNullException>(() => signer.Sign(xmlContent, nullCertificate));
+        }
+
+        [Fact]
+        public void Sign_NFeXmlPassedToMDFeSigner_ThrowsInvalidOperationException()
+        {
+            string nfeXmlContent = File.ReadAllText(_nfePath);
+            X509Certificate2 certificate = new X509Certificate2(_certificatePath, _certificatePassword, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            Assert.Throws<InvalidOperationException>(() => signer.Sign(nfeXmlContent, certificate));
+        }
+
+        [Fact]
+        public void Sign_CertificateWithoutPrivateKey_ThrowsInvalidOperationException()
+        {
+            string xmlContent = File.ReadAllText(_mdfePath);
+
+            X509Certificate2 certificate = new X509Certificate2(_certificateInvalidPath);
+            Assert.Null(certificate.GetRSAPrivateKey());
+
+            MDFeXmlSigner signer = new MDFeXmlSigner();
+
+            var ex = Assert.Throws<InvalidOperationException>(() => signer.Sign(xmlContent, certificate));
+            Assert.Contains("O certificado não possui uma chave privada RSA acessível ou compatível para assinatura.", ex.Message);
+        }
+    }
+}
